ea91f283bf7c0ab5b64d7dc74abc8a262237db26
Seanny123 <saubin@uwaterloo.ca>
2015-02-26 21:13:11 -0500

    got second ensemble working

--- SYDE750_ass3/q4.py
+++ SYDE750_ass3/q4.py
@@ -17,6 +17,7 @@ t_range = np.arange(0, 1, dt)
 h_range = np.arange(1000)*dt-0.5
 h = ptsc(h_range, 0.005)
 
+# first ensemble
 max_firing_rates = np.random.uniform(100, 200, n_neurons)
 x_cepts = np.random.uniform(-2, 2, n_neurons)
 gain_signs = np.random.choice([-1, 1], n_neurons)
@@ -29,6 +30,20 @@ for i in range(n_neurons):
 		)
 	)
 ensemble = lif_ensemble(lifs, gain_signs)
+
+# second ensemble
+max_firing_rates = np.random.uniform(100, 200, n_neurons)
+x_cepts = np.random.uniform(-2, 2, n_neurons)
+gain_signs = np.random.choice([-1, 1], n_neurons)
+lifs = []
+for i in range(n_neurons):
+	lifs.append(
+		modified_lif(
+			x_cepts[i],
+			max_firing_rates[i]
+		)
+	)
+end_ensemble = lif_ensemble(lifs, gain_signs)
 
 # this is the goal function to decode
 decode_func = lambda x: 2*x + 1
@@ -48,13 +63,19 @@ for i_n in range(len(lifs)):
 # decode with the modified function (got this from the notes)
 first_decoders, x_hat = get_decoders(A.T, A.shape[0], decode_func(x_vals))
 
-"""
-# check that it works okay
-fig = plt.figure()
-plt.plot(t_range, decode_func(x_vals), label="input")
-plt.plot(t_range, x_hat, label="approximate")
-plt.legend()
-plt.savefig("funky_decoder")
+# get the decoders for the other thing... huh... how do I do that...
+x_vals = np.linspace(-2, 2, t_range.size)
+res = []
+for val in np.nditer(x_vals):
+	res.append(end_ensemble(val))
+res = np.array(res)
+
+# get activities based off of linear function
+A = np.zeros((t_range.size, res.shape[1]))
+for i_n in range(len(lifs)):
+	A[:,i_n] = np.convolve(res[:,i_n], h, mode='same')
+
+end_decoders, x_hat = get_decoders(A.T, A.shape[0], x_vals)
 
 # get the activities from the new input
 input_func = lambda t: t - 1
@@ -67,19 +88,31 @@ A = np.zeros((t_range.size, res.shape[1]))
 for i_n in range(len(lifs)):
 	A[:,i_n] = np.convolve(res[:,i_n], h, mode='same')
 
-# decode them to make sure they aren't totally absurd
-# OH GOD. THEY'RE TERRIBLE. WHAT HAPPENED. ARE THEY SUPPOSED TO BE THIS BAD?
+# decode them and plug them into the next population
 new_x_hat = np.dot(A, first_decoders)
+
+res = []
+for val in np.nditer(new_x_hat):
+	res.append(end_ensemble(val))
+res = np.array(res)
+
+A = np.zeros((t_range.size, res.shape[1]))
+for i_n in range(len(lifs)):
+	A[:,i_n] = np.convolve(res[:,i_n], h, mode='same')
+
+y_hat = np.dot(A, end_decoders)
+
 fig = plt.figure()
 plt.plot(t_range, input_func(t_range), label="input")
 plt.plot(t_range, decode_func(input_func(t_range)), label="actual")
-plt.plot(t_range, new_x_hat, label="approximate")
+plt.plot(t_range, new_x_hat, label="x approximate")
+plt.plot(t_range, y_hat, label="y approximate")
 plt.legend()
 plt.savefig("4_a")
-"""
 
+"""
 # test this method on a bunch of other functions
-def input_func(t_range, steps=2):
+def input_func(t_range, steps=4):
 	y_vals = np.random.uniform(-1, 0, steps)
 	return_vals = []
 	increment = t_range.size/steps
@@ -87,8 +120,10 @@ def input_func(t_range, steps=2):
 		return_vals.append(y_vals[t/increment])
 	return np.array(return_vals)
 
+input_sig = input_func(t_range)
+
 res = []
-for val in np.nditer(input_func(t_range)):
+for val in np.nditer(input_sig):
 	res.append(ensemble(val))
 res = np.array(res)
 
@@ -97,16 +132,18 @@ for i_n in range(len(lifs)):
 	A[:,i_n] = np.convolve(res[:,i_n], h, mode='same')
 
 # decode them to make sure they aren't totally absurd
+# why can't it just track the value -0.5?
 new_x_hat = np.dot(A, first_decoders)
 
 fig = plt.figure()
 plt.plot(t_range, input_func(t_range), label="input")
-plt.plot(t_range, decode_func(input_func(t_range)), label="actual")
+plt.plot(t_range, decode_func(input_sig), label="actual")
 plt.plot(t_range, new_x_hat, label="approximate")
 plt.legend()
 plt.savefig("4_b")
 
-"""
+noise_sig, _ = whitenoise(1, dt, 1, 5, 0)
+
 input_func = lambda t: 0.2*np.sin(6*np.pi*t)
 
 res = []
--- SYDE750_ass3/utils.py
+++ SYDE750_ass3/utils.py
@@ -47,28 +47,6 @@ def modified_lif(x0_fire, max_fire, t_ref=0.002, t_rc=0.02, radius=2.0):
 
 	return lif_current
 
-def additive_lif(x_inter, max_fire, t_ref=0.002, t_rc=0.02):
-	beta = 1.0 / (
-		1.0 - np.exp(
-			(-1.0/max_fire + t_ref) / t_rc
-		)
-	)
-	alpha = (1.0 - beta)/(x_inter + 1.0)
-	J_bias = 1.0 - alpha * x_inter
-	def lif(x):
-		# dot product with the decoders?
-		J = x * alpha + J_bias
-		return_val = np.zeros(x.shape[0])
-		# Select all the values where J > 1
-		return_val[J > 1] += np.maximum(
-						# Caluclate the activity
-						1.0/(t_ref-t_rc*np.log(1-1/J[J > 1])),
-						# make it zero if it's below zero
-						np.zeros(return_val[J > 1].size)
-					)
-		return return_val
-	return lif
-
 def get_activities(neuron_type, x_vals, n_neurons, x_cepts, max_firing_rates, gain_signs, radius=1.0):
 	neuron_list = []
 	A = np.zeros((n_neurons, x_vals.size))
